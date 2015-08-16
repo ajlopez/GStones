26e3c54d926616be726609b0ac6aa2620a88b834
ajlopez <webmaster@ajlopez.com>
2015-08-16 10:33:21 -0300

    Put stone on current board cell

--- test/boards.js
+++ test/boards.js
@@ -16,7 +16,7 @@ exports['no stones'] = function (test) {
     test.equal(board.hasStones(Color.Black), false);
     test.equal(board.hasStones(Color.Red), false);
     test.equal(board.hasStones(Color.Green), false);
-}
+};
 
 exports['count of stones'] = function (test) {
     var board = boards.createBoard();
@@ -25,4 +25,58 @@ exports['count of stones'] = function (test) {
     test.equal(board.countStones(Color.Black), 0);
     test.equal(board.countStones(Color.Red), 0);
     test.equal(board.countStones(Color.Green), 0);
+};
+
+exports['put blue stone'] = function (test) {
+    var board = boards.createBoard();
+    
+    board.putStone(Color.Blue);
+    
+    test.equal(board.hasStones(Color.Blue), true);
+    test.equal(board.hasStones(Color.Black), false);
+    test.equal(board.hasStones(Color.Red), false);
+    test.equal(board.hasStones(Color.Green), false);
+
+    test.equal(board.countStones(Color.Blue), 1);
+    test.equal(board.countStones(Color.Black), 0);
+    test.equal(board.countStones(Color.Red), 0);
+    test.equal(board.countStones(Color.Green), 0);
 }
+
+exports['put two blue stones'] = function (test) {
+    var board = boards.createBoard();
+    
+    board.putStone(Color.Blue);
+    board.putStone(Color.Blue);
+    
+    test.equal(board.hasStones(Color.Blue), true);
+    test.equal(board.hasStones(Color.Black), false);
+    test.equal(board.hasStones(Color.Red), false);
+    test.equal(board.hasStones(Color.Green), false);
+
+    test.equal(board.countStones(Color.Blue), 2);
+    test.equal(board.countStones(Color.Black), 0);
+    test.equal(board.countStones(Color.Red), 0);
+    test.equal(board.countStones(Color.Green), 0);
+}
+
+exports['put stones of four colors'] = function (test) {
+    var board = boards.createBoard();
+    
+    board.putStone(Color.Blue);
+    board.putStone(Color.Black);
+    board.putStone(Color.Red);
+    board.putStone(Color.Green);
+    
+    test.equal(board.hasStones(Color.Blue), true);
+    test.equal(board.hasStones(Color.Black), true);
+    test.equal(board.hasStones(Color.Red), true);
+    test.equal(board.hasStones(Color.Green), true);
+
+    test.equal(board.countStones(Color.Blue), 1);
+    test.equal(board.countStones(Color.Black), 1);
+    test.equal(board.countStones(Color.Red), 1);
+    test.equal(board.countStones(Color.Green), 1);
+}
+
+
