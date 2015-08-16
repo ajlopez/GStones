0f30995b70bed005703655d706cbefeef031c4cc
ajlopez <webmaster@ajlopez.com>
2015-08-16 17:25:50 -0300

    Colors in machine context

--- test/machines.js
+++ test/machines.js
@@ -1,5 +1,6 @@
 
 var machines = require('../lib/machines');
+var Color = require('../lib/boards').Color;
 
 exports['create machine with context and board'] = function (test) {
     var machine = machines.machine();
@@ -7,5 +8,14 @@ exports['create machine with context and board'] = function (test) {
     test.ok(machine);
     test.ok(machine.board);
     test.ok(machine.context);
+};
+
+exports['defined colors'] = function (test) {
+    var machine = machines.machine();
+    
+    test.equal(machine.context.get('Azul'), Color.Blue);
+    test.equal(machine.context.get('Negro'), Color.Black);
+    test.equal(machine.context.get('Rojo'), Color.Red);
+    test.equal(machine.context.get('Verde'), Color.Green);
 };
 
