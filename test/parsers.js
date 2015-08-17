a82bcbe83ef7dec2fb89724980809aef79145352
ajlopez <webmaster@ajlopez.com>
2015-08-17 16:19:41 -0300

    Parse command, first test

--- test/parsers.js
+++ test/parsers.js
@@ -1,6 +1,8 @@
 
 var parsers = require('../lib/parsers');
 var contexts = require('../lib/contexts');
+var machines = require('../lib/machines');
+var Color = require('../lib/boards').Color;
 
 exports['parse integer constant expression'] = function (test) {
     var parser = parsers.parser('42');
@@ -62,3 +64,18 @@ exports['parse call with two arguments'] = function (test) {
     
     test.equal(parser.parseExpression(), null);
 };
+
+exports['parse command'] = function (test) {
+    var parser = parsers.parser('Poner(Rojo);');
+    
+    var cmd = parser.parseCommand();
+    var machine = machines.machine();
+    
+    test.ok(cmd);
+    
+    cmd.execute(machine.context);
+    
+    test.equal(machine.board.countStones(Color.Red), 1);
+    test.equal(parser.parseCommand(), null);
+};
+
