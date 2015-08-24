d9a7df474874ed699d6af5bedcce0a2b9d4455af
ajlopez <webmaster@ajlopez.com>
2015-08-24 07:51:33 -0300

    Additional test expression command

---
+++ test/commands.js
@@ -0,0 +1,16 @@
+
+var commands = require('../lib/commands');
+var expressions = require('../lib/expressions');
+var contexts = require('../lib/contexts');
+
+exports['create and execute expression command'] = function (test) {
+    var ctx = contexts.context();
+    var done = false;
+    
+    ctx.set('add', function (x, y) { done = true; return x + y; });
+    var cmd = commands.expression(expressions.call('add', [ expressions.constant(1), expressions.constant(2) ]));
+    
+    test.ok(cmd);
+    cmd.execute(ctx);
+    test.ok(done);
+};
