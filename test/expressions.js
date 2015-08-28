8354faf15a32316cfd4df046f322e4fb3a855f1b
ajlopez <webmaster@ajlopez.com>
2015-08-28 07:49:52 -0300

    Add expression

--- test/expressions.js
+++ test/expressions.js
@@ -22,3 +22,12 @@ exports['evaluate call in context'] = function (test) {
     test.equal(expressions.call('add', [ expressions.constant(1), expressions.constant(2) ]).evaluate(ctx), 3);
 };
 
+exports['evaluate add in context'] = function (test) {
+    var ctx = contexts.context();
+    
+    ctx.set('a', 1);
+    ctx.set('b', 2);
+    
+    test.equal(expressions.add(expressions.name('a'), expressions.name('b')).evaluate(ctx), 3);
+};
+
